597a73eb9b8e5af7313b67b04390b988474d4b21
websurfcz <68242375+websurfcz@users.noreply.github.com>
2020-07-13 23:29:50 +0545

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ from setuptools import setup
 with open("src/flask/__init__.py", encoding="utf8") as f:
     version = re.search(r'__version__ = "(.*?)"', f.read()).group(1)
 
+
 # Metadata goes in setup.cfg. These are here for GitHub's dependency graph.
 setup(
     name="Flask",
